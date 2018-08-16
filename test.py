6927d12878c3706d1b303f295da67170cbe3ecab
shahsanket100 <shahsanket100@gmail.com>
2018-08-16 14:38:43 +0530

    added test

---
+++ test.py
@@ -0,0 +1,16 @@
+def qwer():
+    pass
+
+def asdf():
+    print('perform oper')
+    print('going to qwer')
+    qwer()
+    print('baaki ka opr')
+
+asdf()
+qwer()
+
+def zxcv(a='asdf', b='qwer'):
+    print(a, b)
+
+zxcv()
